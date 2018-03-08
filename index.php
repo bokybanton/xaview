8492d4cedf09490eff9a5dcfa8a45b7c201b2cd2
bokybanton <bokyone@gmail.com>
2018-03-08 01:17:57 +0100

    Initial commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+		<title>Xadrez viewer</title>
+		
+	</head>
+	
+	<body>
+		<h1><b>Xa</b>drez <b>view</b>er</h1>
+		<hr>
+		<p><b>Xaview</b> é un acrónimo das palabras «Xadrez» e «viewer» (do inglés visor)</p>
+		<p>Ë un proxecto de código aberto para xestionar partidas de xadrez</p>
+		<hr>
+		<h2>Dependencias</h2>
+		<hr>
+		<ul>
+			<li><a href="https://getbootstrap.com">Bootstrap 4</a></li>
+			<li><a href="https://jquery.com">JQuery</a></li>
+			<li><a href="">Chessboard.js</a></li>
+			<li><a href="">Chess.js</a></li>
+		</ul>
+		<p>
+			<h2>Código fonte</h2>
+			<hr>
+			<p><a href="https://www.github.com/">github/xaview</a></p>
+			
+	</body>
+</html>
